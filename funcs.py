a4fd58a45eb217fb13bb0781f86cea2ec58a3182
WANGHanshuo1220 <whs13720400638@163.com>
2022-01-20 17:42:51 +0100

    finish test and func prototype

---
+++ funcs.py
@@ -0,0 +1,16 @@
+import math
+
+def miroir(s, indice):
+    return -1
+
+def derive(list, interval):
+    return -1
+
+def derive2(list, interval):
+    return -1
+
+def appro_derive(func, ordre, point):
+    return -1
+
+
+    
