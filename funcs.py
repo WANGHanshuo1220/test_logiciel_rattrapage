0878a83d123a2d35444aae8dae16742833044dbc
WANGHanshuo1220 <whs13720400638@163.com>
2022-01-20 17:49:28 +0100

    finish func appro_derive and some funcs and test pass

--- funcs.py
+++ funcs.py
@@ -23,6 +23,24 @@ def derive2(list, interval):
     return derive(der1, interval)
 
 def appro_derive(func, ordre, point):
-    return -1
+    re = func(point)
+    a = len(str(ordre).split(".")[1])
+    re = round(re, a)
+    return re
+
+def func_x2(x):
+    return pow(x, 2)
+
+def func_polynomial(x):
+    return pow(x, 3) + 2 * pow(x, 2) + 3 * x + 1
+
+def func_x_1(x):
+    if x != 0:
+        return 1/x
+    else:
+        return False 
+
+def func_sin(x):
+    return math.sin(x)
 
     
--- test_funcs.py
+++ test_funcs.py
@@ -19,11 +19,11 @@ class TestFuncs(unittest.TestCase):
         self.assertEqual(funcs.derive2([1.0, 2.0, 4.0], 2.0) ,[0.25])
         self.assertEqual(funcs.derive2([1.0, 2.0, 4.0, 10.0, 20.0], 4.0),[0.0625, 0.25, 0.25])
 
-    # def test_appro_derive(self):
-    #     self.assertEqual(funcs.appro_derive(funcs.func_x2, 0.1, 1.2), 1.4)
-    #     self.assertEqual(funcs.appro_derive(funcs.func_polynomial, 0.01, 1.2), 9.21)
-    #     self.assertEqual(funcs.appro_derive(funcs.func_x_1, 0.001, 3.1), 0.323)
-    #     self.assertEqual(funcs.appro_derive(funcs.func_sin, 0.0001, 2), 0.9093)
+    def test_appro_derive(self):
+        self.assertEqual(funcs.appro_derive(funcs.func_x2, 0.1, 1.2), 1.4)
+        self.assertEqual(funcs.appro_derive(funcs.func_polynomial, 0.01, 1.2), 9.21)
+        self.assertEqual(funcs.appro_derive(funcs.func_x_1, 0.001, 3.1), 0.323)
+        self.assertEqual(funcs.appro_derive(funcs.func_sin, 0.0001, 2), 0.9093)
 
 if __name__ == '__main__':
 	unittest.main()
