5cf49a34b60279abc30f8b233dc6d33cc8d800b0
WANGHanshuo1220 <whs13720400638@163.com>
2022-01-26 10:46:19 +0100

    Ajout de commentaires de code

--- funcs.py
+++ funcs.py
@@ -6,6 +6,8 @@ def miroir(s, indice):
     re = s1 + s2
     return re
 
+# “list” est l'ensemble des positions des boules à un moment donné
+# “interval” est l'intervalle d'échantillonnage.
 def derive(list, interval):
     re = []
 
@@ -17,28 +19,38 @@ def derive(list, interval):
         re.append(derive)
 
     return re
+# Renvoie les vitesses moyennes d'un objet en mouvement (dérivée de premier ordre)
+
 
 def derive2(list, interval):
     der1 = derive(list, interval)
-    return derive(der1, interval)
+    if der1:
+        return derive(der1, interval)
+    else:
+        return False
+        
 
-def appro_derive(func, ordre, point):
+def appro_derive(func, point, ordre):
     re = func(point)
     a = len(str(ordre).split(".")[1])
     re = round(re, a)
     return re
 
+
 def func_x2(x):
     return pow(x, 2)
 
+
 def func_polynomial(x):
     return pow(x, 3) + 2 * pow(x, 2) + 3 * x + 1
+
 
 def func_x_1(x):
     if x != 0:
         return 1/x
     else:
         return False 
+
 
 def func_sin(x):
     return math.sin(x)
--- test_funcs.py
+++ test_funcs.py
@@ -6,11 +6,12 @@ class TestFuncs(unittest.TestCase):
     def test_miroir(self):
         self.assertEqual(funcs.miroir("WeAreTheChampion", 3),"WeArrAeW")
         self.assertEqual(funcs.miroir("WeAreTheChampion", 6),"WeAreThhTerAeW")
+        self.assertEqual(funcs.miroir("a", 1),"aa")
 
     def test_derive(self):
         self.assertEqual(funcs.derive([1.0, 2.0], 2.0), [0.5])
         self.assertEqual(funcs.derive([1.0, 2.0, 4.0], 2.0), [0.5, 1.0])
-        self.assertEqual(funcs.derive([1.0, 2.0, 4.0, 10.0, 20.0], 4.0), [0.25, 0.5, 1.5, 2.5])
+        self.assertEqual(funcs.derive([1.0, 2.0, 4.0, 10.0, 20.0 ], 4.0), [0.25, 0.5, 1.5, 2.5])
         self.assertEqual(funcs.derive([1.0], 2.0), False)
         self.assertEqual(funcs.derive([], 2.0), False)
 
@@ -18,12 +19,15 @@ class TestFuncs(unittest.TestCase):
     def test_derive2(self):
         self.assertEqual(funcs.derive2([1.0, 2.0, 4.0], 2.0) ,[0.25])
         self.assertEqual(funcs.derive2([1.0, 2.0, 4.0, 10.0, 20.0], 4.0),[0.0625, 0.25, 0.25])
+        self.assertEqual(funcs.derive2([1.0, 2.0], 2.0) ,False)
+        self.assertEqual(funcs.derive2([1.0], 2.0) ,False)
+
 
     def test_appro_derive(self):
-        self.assertEqual(funcs.appro_derive(funcs.func_x2, 0.1, 1.2), 1.4)
-        self.assertEqual(funcs.appro_derive(funcs.func_polynomial, 0.01, 1.2), 9.21)
-        self.assertEqual(funcs.appro_derive(funcs.func_x_1, 0.001, 3.1), 0.323)
-        self.assertEqual(funcs.appro_derive(funcs.func_sin, 0.0001, 2), 0.9093)
+        self.assertEqual(funcs.appro_derive(funcs.func_x2, 1.2, 0.1), 1.4) 
+        self.assertEqual(funcs.appro_derive(funcs.func_polynomial, 1.2, 0.01), 9.21)
+        self.assertEqual(funcs.appro_derive(funcs.func_x_1, 3.1, 0.001), 0.323)
+        self.assertEqual(funcs.appro_derive(funcs.func_sin, 2, 0.0001), 0.9093)
 
 if __name__ == '__main__':
 	unittest.main()
